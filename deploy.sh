18eb5d8354040737ab3f98f56a9015eb807bb8b2
Mihir Choudhury <choudhury@us.ibm.com>
2021-08-17 10:54:59 -0400

    aca -> tca

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
-ACA_GITHUB_REPO=https://github.com/mihirc-github/tackle-container-advisor
-ACA_GITHUB_BRANCH=os_kg_utils
-ACA_GITHUB_DIR=aca_backend_api
-ACA_DEPLOY_NAME=aca
-OCP4_ACCESS_TOKEN=sha256~xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+TCA_GITHUB_REPO=https://github.com/mihirc-github/tackle-container-advisor
+TCA_GITHUB_BRANCH=os_kg_utils
+TCA_GITHUB_DIR=aca_backend_api
+TCA_DEPLOY_NAME=tca
+OCP4_ACCESS_TOKEN=sha256~XwNJ1b-Yjkduk-d8xgKeis1k_j2s2PnRNqvi6T8GV7c 
 OCP4_API_SERVER=https://api.sandbox-m2.ll9k.p1.openshiftapps.com:6443
 
 
 oc login --token=${OCP4_ACCESS_TOKEN} --server=${OCP4_API_SERVER}
 oc projects
 oc delete all --selector  app=aca
-echo "Creating application from ${ACA_GITHUB_REPO}/${ACA_GITHUB_DIR}#${ACA_GITHUB_BRANCH}"
-oc new-app ${ACA_GITHUB_REPO}#${ACA_GITHUB_BRANCH} --context-dir=${ACA_GITHUB_DIR} --name ${ACA_DEPLOY_NAME}
-oc expose service ${ACA_DEPLOY_NAME}
-oc get route ${ACA_DEPLOY_NAME}
+oc delete all --selector  app=tca
+echo "Creating application from ${TCA_GITHUB_REPO}/${TCA_GITHUB_DIR}#${TCA_GITHUB_BRANCH}"
+oc new-app ${TCA_GITHUB_REPO}#${TCA_GITHUB_BRANCH} --context-dir=${TCA_GITHUB_DIR} --name ${TCA_DEPLOY_NAME}
+oc expose service ${TCA_DEPLOY_NAME}
+oc get route ${TCA_DEPLOY_NAME}
