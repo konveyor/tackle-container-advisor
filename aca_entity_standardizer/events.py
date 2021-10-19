2a294b170f3c3e615616e7d733dc5cce8d5b787a
Mihir Choudhury <choudhury@us.ibm.com>
2021-10-19 11:59:16 -0400

    Updated

--- aca_entity_standardizer/events.py
+++ aca_entity_standardizer/events.py
@@ -5,4 +5,6 @@ if __name__ == '__main__':
     try:
         events = json.loads(sys.argv[1])
     except Exception as e:
-        print(sys.argv[1])
+        print("Exception = ", e, sys.argv[1])
+   
+    print(events["head_commit"]["timestamp"], events["head_commit"]["url"])
