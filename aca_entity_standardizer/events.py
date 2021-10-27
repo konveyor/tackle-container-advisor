7d0c5af86b5d9da4b35554433c30c472a13c36f7
Mihir Choudhury <choudhury@us.ibm.com>
2021-10-27 14:55:35 -0400

    Safe events

--- aca_entity_standardizer/events.py
+++ aca_entity_standardizer/events.py
@@ -1,14 +1,17 @@
 import json
 import sys
+from datetime import datetime
 
 if __name__ == '__main__':
     try:
         events = json.loads(sys.argv[1])
+        date   = events["head_commit"]["timestamp"].split('T')[0]
+        url    = events["head_commit"]["url"]
     except Exception as e:
-        print("Exception = ", e, sys.argv[1])
-   
-    date = events["head_commit"]["timestamp"].split('T')[0]
-    url  = events["head_commit"]["url"]
+        date   = datetime.today().strftime('%Y-%m-%d')
+        url    = ""
+        print("Exception = ", e)
+
     print("----------- COPY MESSAGE BELOW AND PASTE INTO README -------------")
     print("")
     print("")
