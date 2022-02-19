1b0ec2c1e5467be3fec5b6fe8da66991119eb8af
mmerler <michele.merler@gmail.com>
2022-02-18 19:07:34 -0500

    updated requirements.txt with itsdangerous==2.1.0 compatible with flask

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ if [ "$( docker container inspect -f '{{.State.Status}}' aca_backend_api )" == "
     docker rmi aca_backend_api
 fi
 
-docker-compose  -f 'docker-compose-api.yml' --env-file ./config.ini up -d --build
+docker-compose  -f 'docker-compose-api.yml' up -d --build
 
 cd ..
