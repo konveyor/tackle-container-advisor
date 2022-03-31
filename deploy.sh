d52faa467ce4509c27220d1cf4c3cadbe31433ba
Mihir Choudhury <choudhury@us.ibm.com>
2022-03-31 16:02:00 -0400

    Updated to point to konveyor

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-TCA_GITHUB_REPO=https://github.com/mihirc-github/tackle-container-advisor
+TCA_GITHUB_REPO=https://github.com/konveyor/tackle-container-advisor
 TCA_GITHUB_BRANCH=main
 TCA_DEPLOY_NAME=tca
-OCP4_ACCESS_TOKEN=sha256~xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+OCP4_ACCESS_TOKEN=sha256~XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 OCP4_API_SERVER=https://api.sandbox.x8i5.p1.openshiftapps.com:6443
 
 
