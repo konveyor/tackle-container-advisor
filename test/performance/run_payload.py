3087508ca6febbebdbaf84c98a222d7ef290c9d6
MIHIR CHOUDHURY <choudhury@us.ibm.com>
2022-12-07 17:21:46 -0500

    Payload generation bug fix
    Signed-off-by: MIHIR CHOUDHURY <choudhury@us.ibm.com>

--- test/performance/run_payload.py
+++ test/performance/run_payload.py
@@ -9,29 +9,27 @@ import time
 import subprocess
 
 def call_tca(url):
+    global data
     start = time.time()
     headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
     resp = requests.post(url, data=json.dumps(data), headers=headers)
     return time.time()-start,resp
 
 def get_performance_data(n_user, n_data):
-    global data
-    actual_len = len(data)
+    global alldata, data
+    actual_len = len(alldata)
     if n_data != 0:
         required_data = n_data
     else:
         required_data = actual_len
     quo = required_data // actual_len
     rem = required_data % actual_len
-    data = quo * data + data[:rem]
-    # print('Payload size : ', len(data), 'records')
+    data = quo * alldata + alldata[:rem]
 
     assert(n_user > 0)
-    # print('No of concurrent user : ',n_user)
   
     urls = [host+':'+port+'/standardize']*n_user  
 
-    # os.system('podman run --name tca -d -p 8000:8000 -m 4000m quay.io/konveyor/tackle-container-advisor:v1.1.0 > /dev/null')
     with open(os.devnull, 'wb') as devnull:
         cmd = ctrserv + ' run --name tca -d -p 8000:'+port+ ' -m '+memlim+' '+image+':'+version
         subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=subprocess.STDOUT)
@@ -65,11 +63,14 @@ def get_performance_data(n_user, n_data):
     with open(os.devnull, 'wb') as devnull:
         cmd = ctrserv + ' stats --no-stream --format=json tca > stats.json'
         subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=subprocess.STDOUT)
+        cmd = ctrserv + ' exec -it tca grep \"unique mentions\" /app/gunicorn.debug.log'
+        subprocess.check_call(cmd, shell=True)
         cmd = ctrserv + ' stop tca'
         subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=subprocess.STDOUT)
         cmd = ctrserv + ' rm tca'
         subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=subprocess.STDOUT)
-    
+
+
     with open('stats.json', 'r') as stats_file:
         stats = json.load(stats_file)
         result['server'] = stats[0]
@@ -92,9 +93,10 @@ except KeyError as k:
     logging.error(f'{k}  is not a key in your test.ini file.')
     exit()
 
-data = None
+data    = None
+alldata = None
 with open(os.path.join("test", "performance", file)) as f:
-    data = json.load(f)
+    alldata = json.load(f)
 
 dpts    = [(1,10), (1,100), (1,1000), (1,5000)]
 results = []
@@ -102,6 +104,7 @@ for pt in dpts:
     print("Working on", pt)
     result = get_performance_data(pt[0], pt[1])
     results.append(result)
+print(results)
 with open(os.path.join("test", "performance", "results.json"), 'w') as f:
     json.dump(results, f, indent=4)
 
